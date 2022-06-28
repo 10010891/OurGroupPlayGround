43f46c62688269bde49c7de8b7338fa1f0ccb4b8
Md Ibrahim <106436211+10010891@users.noreply.github.com>
2022-06-28 13:59:17 +0600

    proper javascript code

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-this text will also be visiible on github 
-now i will just edit few lines and commit 
-this file to github 
-our eediting done now i will commit this file
-to github
-
-so you see that it is very easy to work with github repositories
-
-thats all for now .
-have a nice time 
-byee....
-
+console.log('hello world!')
