b130dea5e6418c0c22d77f692c7db6380e5391c6
Md Ibrahim <106436211+10010891@users.noreply.github.com>
2022-07-04 13:56:04 +0600

    update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function toggleSideMenu(){
+function toggleSideMenu() {
     var sideNav = document.getElementById("nav")
     if (sideNav.style.display == "none") {
         sideNav.style.display = "block"
@@ -6,11 +6,33 @@ function toggleSideMenu(){
         sideNav.style.display = "none"
     }
 }
-function closeSideMenu(){
+
+function closeSideMenu() {
     var sideNav = document.getElementById("nav")
     if (sideNav.style.display == "none") {
         sideNav.style.display = "block"
     } else {
         sideNav.style.display = "none"
     }
+}
+
+var amountBox = document.getElementById("amount")
+function increaseAmount(){
+    var temp = Number(amountBox.value)
+    temp += 1
+    amountBox.value = temp 
+    console.log(amountBox.value)
+}
+function decreaseAmount() {
+    if (amountBox.value > 0){
+        var temp = Number(amountBox.value)
+        temp -= 1
+        amountBox.value = temp
+        console.log(amountBox.value)
+    }else{
+        alert("You cannot select less than 0 amount of Product...")
+    }
+}
+function addToCart(){
+    alert("This Page is readOnly.It's not connected with server yet.")
 }
